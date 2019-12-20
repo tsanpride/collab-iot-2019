cb296c93811479e62183f97932ee83c1f640a00d
pendekar-sawo <58844177+pendekar-sawo@users.noreply.github.com>
2019-12-20 16:04:20 +0700

    halaman utama

---
+++ index edit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LORDJALAL SMART DOOR</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px comic; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Selamat Datang di Lordjalal Smart Door</h1>
+    </div>
+    <p>
+        <a href="register.php" class="btn btn-warning">Daftar</a>
+        <a href="login.php" class="btn btn-danger">Masuk</a>
+    </p>
+</body>
+</html>
